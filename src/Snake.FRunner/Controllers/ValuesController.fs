98ad72cf80d6756aed2937980ce8f1a4738e29fd
Valerii <akai.no.ishi@gmail.com>
2018-02-12 21:50:31 +0100

    add f# app

---
+++ src/Snake.FRunner/Controllers/ValuesController.fs
@@ -0,0 +1,11 @@
+﻿namespace Snake.FRunner.Controllers
+
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+type ValuesController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member __.Get() =
+        ["1", "2"]
